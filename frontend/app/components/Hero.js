959f7580a987f939217f8a215fbc5a2ff8f9dffc
Daniel <cawasdn@gmail.com>
2025-04-10 10:00:05 +0800

    Added Hero section

--- frontend/app/components/Hero.js
+++ frontend/app/components/Hero.js
@@ -1,10 +1,36 @@
-const Navigation = () => {
-    return ( 
+import Image from "next/image";
+import { FaLocationDot } from "react-icons/fa6";
 
-        <div>
-            
+const Navigation = () => {
+  return (
+    <>
+      <div className="h-[10px] w-full bg-[#EC1D23]"></div>
+      <nav className="h-full bg-slate-300">
+        <div className="flex justify-between p-5">
+          <Image
+            src="/images/logo.png"
+            alt="Pharmacy Logo"
+            width={200}
+            height={100}
+          />
+          <div className="p-3 bg-red-500 rounded-md">
+            <FaLocationDot color="white" size={15} />
+          </div>
         </div>
-    );
-}
- 
+        <div className="hidden">
+          <ul>
+            <li>Store Locator</li>
+          </ul>
+          <ul>
+            <li>Order Online</li>
+          </ul>
+          <ul>
+            <li>Drug Information</li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
 export default Navigation;
